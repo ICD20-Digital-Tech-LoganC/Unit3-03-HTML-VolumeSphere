c7a16fb314ca9140c1761f3cbbd493ed2d68fc80
Logan-T-Connors <151658201+Logan-T-Connors@users.noreply.github.com>
2024-04-09 15:05:57 +0000

    final webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+/* JavaScript code to calculate and display the volume of a sphere */
+
+function calculateVolume() {
+    const radius = parseFloat(document.getElementById('radiusInput').value);
+
+    if (!isNaN(radius)) {
+        const volume = (4/3) * Math.PI * Math.pow(radius, 3);
+        document.getElementById('volumeResult').textContent = `The volume of the sphere is: ${volume.toFixed(3)}`;
+    } else {
+        document.getElementById('volumeResult').textContent = 'Please enter a valid number for the radius.';
+    }
+}
